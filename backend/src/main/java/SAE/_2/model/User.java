a9f17b0f1591838b5fb90ce39957ec16a29d7e8e
yohan <61011701+yoh4nyo@users.noreply.github.com>
2026-03-10 19:44:26 +0100

    Fix: quote reserved keyword 'user' table for PostgreSQL

--- backend/src/main/java/SAE/_2/model/User.java
+++ backend/src/main/java/SAE/_2/model/User.java
@@ -5,15 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "\"user\"")
 public class User{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String identifiant;
-    
+
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String role;
